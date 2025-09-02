6616194ee812583de65ad12115164f500b732ff3
fahmi <effendif157@gmail.com>
2025-09-02 07:40:29 +0700

    buatkan folder conditional

---
+++ conditional/materi/ifelse.php
@@ -0,0 +1,13 @@
+<?php
+        $nilai = 100;
+
+        if ($nilai >= 90) {
+            echo "nilai anda A";
+        }elseif ($nilai >=75) {
+            echo "nilai anda B";
+        }elseif ($nilai >=60) {
+            echo "nilai anda C";
+        }else {
+            echo "nilai anda D";
+        }
+    ?>
